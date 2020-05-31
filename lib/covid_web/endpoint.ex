485840ca2a9bd2d2b02c8b054fd9419b1024ff1d
Eric Iacutone <eric.iacutone@gmail.com>
2020-05-31 12:56:41 -0400

    Add Heroku config stuff

--- lib/covid_web/endpoint.ex
+++ lib/covid_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule CovidWeb.Endpoint do
   ]
 
   socket "/socket", CovidWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Covid.MixProject do
     [
       app: :covid,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.9.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
